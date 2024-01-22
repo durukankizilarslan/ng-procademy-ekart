b37a83a8d5e48be9a623065aab3f407fa0702092
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-01-22 14:00:44 +0300

    button functionalities added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,16 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  name: string = "John Doe";
-  product = { name:'iPhone',
-  price: 999,
-  color:'Matte Black',
-  discount: 8,
-  inStock: 5,
-  pImage: "../../assets/iphone-png.png"}
+  //name: string = "John Doe";
+  addToCart: number = 0;
+  product = {
+    name: 'iPhone',
+    price: 999,
+    color: 'Matte Black',
+    discount: 8,
+    inStock: 5,
+    pImage: "../../assets/iphone-png.png"
+  }
 
   getDiscountedPrice() {
     return this.product.price - (this.product.price * this.product.discount) / 100
   }
- 
+
+  onNameChange(event: any) {
+    //this.name = event.target.value;
+    //console.log(event.target.value);
+  }
+
+  incrementCartValue() {
+    if (this.addToCart < this.product.inStock) {
+      this.addToCart++;
+    }
+  }
+
+  decrementCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+
 }
