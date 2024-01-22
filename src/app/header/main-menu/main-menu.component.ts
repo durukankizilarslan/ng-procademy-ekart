8408a71d76a31de457681bc5ccefbae617a9a7a8
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-01-23 00:36:26 +0300

    ngFor iteration added

--- src/app/header/main-menu/main-menu.component.ts
+++ src/app/header/main-menu/main-menu.component.ts
@@ -1,12 +1,13 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.css'
 })
 export class MainMenuComponent {
-
+  mainMenuItems: string[] = ['Home', 'Products', 'About', 'Contact'];
 }
