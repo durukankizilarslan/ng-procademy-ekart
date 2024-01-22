32b43bdc173346a7b40a8c824ce462e6f4b40928
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-01-23 00:43:21 +0300

    ngFor iteration added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,45 +1,46 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from './search/search.component';
-
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'product-list',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent, NgFor],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  listOfItems: string[] = ['Sam', "Frank", 'Herkel', "Tony", "Johny"]
   //name: string = "John Doe";
-  addToCart: number = 0;
-  product = {
-    name: 'iPhone',
-    price: 999,
-    color: 'Matte Black',
-    discount: 8,
-    inStock: 5,
-    pImage: "../../assets/iphone-png.png"
-  }
+  // addToCart: number = 0;
+  // product = {
+  //   name: 'iPhone',
+  //   price: 999,
+  //   color: 'Matte Black',
+  //   discount: 8,
+  //   inStock: 5,
+  //   pImage: "../../assets/iphone-png.png"
+  // }
 
-  getDiscountedPrice() {
-    return this.product.price - (this.product.price * this.product.discount) / 100
-  }
+  // getDiscountedPrice() {
+  //   return this.product.price - (this.product.price * this.product.discount) / 100
+  // }
 
-  onNameChange(event: any) {
-    //this.name = event.target.value;
-    //console.log(event.target.value);
-  }
+  // onNameChange(event: any) {
+  //   //this.name = event.target.value;
+  //   //console.log(event.target.value);
+  // }
 
-  incrementCartValue() {
-    if (this.addToCart < this.product.inStock) {
-      this.addToCart++;
-    }
-  }
+  // incrementCartValue() {
+  //   if (this.addToCart < this.product.inStock) {
+  //     this.addToCart++;
+  //   }
+  // }
 
-  decrementCartValue() {
-    if (this.addToCart > 0) {
-      this.addToCart--;
-    }
-  }
+  // decrementCartValue() {
+  //   if (this.addToCart > 0) {
+  //     this.addToCart--;
+  //   }
+  // }
 
 }
