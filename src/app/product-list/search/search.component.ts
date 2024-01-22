c5dcb1e1ab43325cc9462e15db73991f9c366cb7
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-01-22 17:32:05 +0300

    class data added

--- src/app/product-list/search/search.component.ts
+++ src/app/product-list/search/search.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  searchText: string = "Mens wear";
+
+
 
 }
