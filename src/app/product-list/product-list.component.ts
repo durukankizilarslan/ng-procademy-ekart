d7ab737a93f513cb439feda38755cfbeca613c92
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-01-22 12:59:40 +0300

    product list component added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  name: string = "John Doe";
+  product = { name:'iPhone',
+  price: 999,
+  color:'Matte Black',
+  discount: 8,
+  inStock: 5,
+  pImage: "../../assets/iphone-png.png"}
 
+  getDiscountedPrice() {
+    return this.product.price - (this.product.price * this.product.discount) / 100
+  }
+ 
 }
