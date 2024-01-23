227ec5eb48487a9717ca81a54a579fed25652be8
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-01-23 13:46:43 +0300

    ngIf and ngClass added

--- src/app/product-list/search/search.component.ts
+++ src/app/product-list/search/search.component.ts
@@ -1,10 +1,11 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, NgClass],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
